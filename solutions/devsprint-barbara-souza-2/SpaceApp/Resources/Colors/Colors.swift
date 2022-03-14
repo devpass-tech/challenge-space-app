e1ae66eab838c69c01be81ecc8cded6a244d8640
Barbara Souza <barbara.souza@tui.co.uk>
2022-03-14 22:12:49 +0000

    Add Color files and dark mode

---
+++ solutions/devsprint-barbara-souza-2/SpaceApp/Resources/Colors/Colors.swift
@@ -0,0 +1,12 @@
+//
+//  Colors.swift
+//  SpaceApp
+//
+//  Created by Bárbara Souza on 14/03/2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static var backgroundColor = UIColor(named: "backgroundColor")
+}

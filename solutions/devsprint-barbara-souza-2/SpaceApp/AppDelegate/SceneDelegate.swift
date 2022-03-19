62e14c12e7e0d486a26f01feae9c44b4fc364d46
Hyago Henrique <hyagoh@icloud.com>
2022-03-18 21:33:12 -0300

    removing configuration on scene delegate

--- solutions/devsprint-barbara-souza-2/SpaceApp/AppDelegate/SceneDelegate.swift
+++ solutions/devsprint-barbara-souza-2/SpaceApp/AppDelegate/SceneDelegate.swift
@@ -22,12 +22,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         self.window?.makeKeyAndVisible()
     }
     
-    private func navigationBarConfiguration(_ controller: UINavigationController) {
-        controller.navigationBar.prefersLargeTitles = true
-        controller.navigationBar.tintColor = .white
-        controller.navigationBar.backgroundColor = .backgroundNextLaunchColor
-    }
-    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
